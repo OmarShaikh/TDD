275afc47641a6b3d3663c1af5999c2a43cec3531
Omar <omarkshaikh@gmail.com>
2023-09-14 22:58:47 +0400

    Added Chapter 5: Franc-ly Speaking

---
+++ src/test/java/FrancTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FrancTest {
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five= new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
