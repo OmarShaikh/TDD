177c786bee43bf4b0f1d7de7977534f9df5de296
Omar <omarkshaikh@gmail.com>
2023-09-18 21:55:46 +0400

    Added Chapter 12: Addition, Finally

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -30,5 +30,9 @@ class Money {
         Money money = (Money) object;
         return amount == money.amount
                 && currency().equals(money.currency());
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
